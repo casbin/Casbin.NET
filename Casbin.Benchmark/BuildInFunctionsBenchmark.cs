80515e0cf375eb1d63b16ac94cd1689f6ca1bee7
Sagilio <Sagilio@outlook.com>
2020-07-30 18:41:31 +0800

    Improve KeyMatch4 function performence
    Signed-off-by: Sagilio <Sagilio@outlook.com>

---
+++ Casbin.Benchmark/BuildInFunctionsBenchmark.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Jobs;
+using NetCasbin.Util;
+
+namespace Casbin.Benchmark
+{
+    [MemoryDiagnoser]
+    [BenchmarkCategory("Functions")]
+    [SimpleJob(RunStrategy.Throughput, targetCount: 10, runtimeMoniker: RuntimeMoniker.Net48)]
+    [SimpleJob(RunStrategy.Throughput, targetCount: 10, runtimeMoniker: RuntimeMoniker.NetCoreApp31, baseline: true)]
+    public class BuildInFunctionsBenchmark
+    {
+        public IEnumerable<object[]> KeyMatch4TestData() => new[]
+        {
+            new object[] {"/parent/123/child/123", "/parent/{id}/child/{id}"},
+            new object[] {"/parent/123/child/123", "/parent/{id}/child/{another_id}"}
+        };
+
+        [Benchmark]
+        [BenchmarkCategory(nameof(KeyMatch4))]
+        [ArgumentsSource(nameof(KeyMatch4TestData))]
+        public void KeyMatch4(string key1, string key2)
+        {
+            _ = BuiltInFunctions.KeyMatch4(key2, key2);
+        }
+    }
+}
