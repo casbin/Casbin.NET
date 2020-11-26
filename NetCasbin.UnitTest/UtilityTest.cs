dd4fda4ae6c62bbcf74d2ff1e4a0f58fb51de560
Sagilio <Sagilio@outlook.com>
2020-11-27 01:25:38 +0800

    Support multiple eval functions
    Signed-off-by: Sagilio <Sagilio@outlook.com>

--- NetCasbin.UnitTest/UtilityTest.cs
+++ NetCasbin.UnitTest/UtilityTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using NetCasbin.Util;
 using Xunit;
 
@@ -31,6 +32,49 @@ namespace NetCasbin.UnitTest
             Assert.Equal("r.act == p.act", Utility.RemoveComments("r.act == p.act###"));
             Assert.Equal("", Utility.RemoveComments("### comments"));
             Assert.Equal("r.act == p.act", Utility.RemoveComments("r.act == p.act"));
+        }
+
+        public static IEnumerable<object[]> replaceEvalTestData = new[]
+        {
+            new object[] {"eval(rule1)", "a == b",
+                new Dictionary<string, string>{["rule1"] = "a == b"}},
+            new object[] {"eval(rule1) && c && d", "a == b && c && d",
+                new Dictionary<string, string>{["rule1"] = "a == b"}},
+
+            new object[] {"eval(rule1)", "eval(rule1)",
+                null},
+            new object[] {"eval(rule1) && c && d", "eval(rule1) && c && d",
+                null},
+
+            new object[] {"eval(rule1) || eval(rule2)", "a == b || a == c",
+                new Dictionary<string, string>{["rule1"] = "a == b", ["rule2"] = "a == c"}},
+            new object[] {"eval(rule1) || eval(rule2) && c && d", "a == b || a == c && c && d",
+                new Dictionary<string, string>{["rule1"] = "a == b", ["rule2"] = "a == c"}},
+
+            new object[] {"eval(rule1) || eval(rule2)", "a == b || eval(rule2)",
+                new Dictionary<string, string>{["rule1"] = "a == b"}},
+            new object[] {"eval(rule1) || eval(rule2) && c && d", "a == b || eval(rule2) && c && d",
+                new Dictionary<string, string>{["rule1"] = "a == b"}},
+
+            new object[] {"eval(rule1) || eval(rule2)", "eval(rule1) || a == c",
+                new Dictionary<string, string>{["rule2"] = "a == c"}},
+            new object[] {"eval(rule1) || eval(rule2) && c && d", "eval(rule1) || a == c && c && d",
+                new Dictionary<string, string>{["rule2"] = "a == c"}},
+
+            new object[] {"eval(rule1) || eval(rule2)", "eval(rule1) || eval(rule2)",
+                null},
+            new object[] {"eval(rule1) || eval(rule2) && c && d", "eval(rule1) || eval(rule2) && c && d",
+                null}
+        };
+
+        [Theory]
+        [MemberData(nameof(replaceEvalTestData))]
+        public void TestReplaceEval(
+            string oldExpressionString,
+            string newExpressionString,
+            IDictionary<string, string> rules)
+        {
+            Assert.Equal(newExpressionString, Utility.ReplaceEval(oldExpressionString, rules));
         }
     }
 }
