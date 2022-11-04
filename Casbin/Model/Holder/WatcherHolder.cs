677e94b83a7fd4ba7e2110eb2b0a8eedf62c9700
sagilio <sagilio@outlook.com>
2022-11-04 23:28:23 +0800

    ci: Ignore pack warming
    Signed-off-by: sagilio <sagilio@outlook.com>

--- Casbin/Model/Holder/WatcherHolder.cs
+++ Casbin/Model/Holder/WatcherHolder.cs
@@ -12,7 +12,7 @@ public class WatcherHolder
         set
         {
             _watcher = value;
-            DetermineWater(value);
+            DetermineWatcher(value);
         }
     }
 
@@ -20,10 +20,11 @@ public class WatcherHolder
     public IIncrementalWatcher IncrementalWatcher { get; private set; }
     public IWatcherEx WatcherEx { get; private set; }
 
-    private void DetermineWater(IReadOnlyWatcher watcher)
+    private void DetermineWatcher(IReadOnlyWatcher watcher)
     {
         FullWatcher = watcher as IFullWatcher;
         IncrementalWatcher = watcher as IIncrementalWatcher;
         WatcherEx = watcher as IWatcherEx;
     }
 }
+
