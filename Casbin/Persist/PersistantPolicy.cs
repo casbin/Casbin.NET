f2671d6ab7d207e8f5177c82b0a3cdb0d326c13a
DacongDA <dacongda1@sina.com>
2024-04-03 23:30:47 +0800

    fix: fix bugs in policy equal and persist

--- Casbin/Persist/PersistantPolicy.cs
+++ Casbin/Persist/PersistantPolicy.cs
@@ -31,7 +31,7 @@ public class PersistPolicy : IPersistPolicy, IReadOnlyPersistPolicy
         where TPersistPolicy : IPersistPolicy, new()
     {
         TPersistPolicy persistPolicy = new TPersistPolicy { Section = section, Type = type };
-        switch (values.Count)
+        switch (values.GetRealCount())
         {
             case 1:
                 persistPolicy.Value1 = values[0];
@@ -136,7 +136,7 @@ public class PersistPolicy : IPersistPolicy, IReadOnlyPersistPolicy
                 persistPolicy.Value12 = values[11];
                 break;
             default:
-                throw new ArgumentOutOfRangeException(nameof(values), values.Count,
+                throw new ArgumentOutOfRangeException(nameof(values), values.GetRealCount(),
                     "The number of values must be between 1 and 12.");
         }
 
